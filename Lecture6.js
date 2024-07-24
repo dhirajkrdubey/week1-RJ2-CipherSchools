bb4256371ee9412b27377c930383e1515b8b73a5
Dhiraj Dubey <dhirajdubey401@gmail.com>
2024-07-24 23:37:28 +0530

    Add files via upload

---
+++ Lecture6.js
@@ -0,0 +1,23 @@
+import React from "react";
+const App = () => {
+    return (
+        <div>
+            <h1>hello, I am Max</h1>
+        </div>
+    );
+};
+export default App;
+
+_______________________________________________________________________
+
+import React, { Component } from "react";
+class Test extends Component {
+    render () {
+        return (
+            <div>
+                <h1> id = "testId>Hello, I am Max</h1>
+            </div>
+        );
+    };
+};
+export default Test;
